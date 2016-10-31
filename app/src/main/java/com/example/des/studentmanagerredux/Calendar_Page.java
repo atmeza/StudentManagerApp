5fb5c141a62cf731d612b52e5f5c90a3f7ec171c
Tyger Yang <Tbyang@ucsd.edu>
2016-10-31 10:03:03 -0700

    Updated gradle files to (hopefully) support Google Calendar API (Undid the last commit)

--- app/src/main/java/com/example/des/studentmanagerredux/Calendar_Page.java
+++ app/src/main/java/com/example/des/studentmanagerredux/Calendar_Page.java
@@ -3,7 +3,10 @@ package com.example.des.studentmanagerredux;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.CalendarView;
+import android.widget.CalendarView.OnDateChangeListener;
+import android.widget.Toast;
 import java.util.Calendar;
+import android.view.View;
 
 public class Calendar_Page extends AppCompatActivity {
 
@@ -11,5 +14,19 @@ public class Calendar_Page extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
+        CalendarView calendarView=(CalendarView) findViewById(R.id.calendarView);
+        calendarView.setOnDateChangeListener(new OnDateChangeListener() {
+
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month,
+                                            int dayOfMonth) {
+                Toast.makeText(getApplicationContext(), ""+dayOfMonth, 0).show();// TODO Auto-generated method stub
+            }
+        });
     }
+
+    void CalendarClick (View view) {
+
+    }
+
 }
