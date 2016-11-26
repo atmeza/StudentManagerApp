f0eef0e064e324e43f33af24a8b82639970c8f8e
aliibrahim <a4ibrahi@ucsd.edu>
2016-11-26 11:30:40 -0800

    ToDo List now updates dynamically whenever a change is made(remove task/change name of task) without needing to leave the page. GPACalculator now has a database, so classes are now saved when leaving the page.

--- app/src/main/java/com/example/des/samplehomescreen/Login.java
+++ app/src/main/java/com/example/des/samplehomescreen/Login.java
@@ -4,15 +4,29 @@ package com.example.des.samplehomescreen;
 
 
 import android.content.Intent;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.des.studentmanagerredux.HomeScreen;
 import com.example.des.studentmanagerredux.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 public class Login extends AppCompatActivity implements View.OnClickListener {
@@ -22,9 +36,27 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
     EditText etUsername, etPassword;
     TextView tvRegisterLink;
 
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private static final String TAG = "Login";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+                // ...
+            }
+        };
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
@@ -38,7 +70,6 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
         bLogin.setOnClickListener(this);
         tvRegisterLink.setOnClickListener(this);
     }
-
 
     @Override
     public void onClick(View v) {
@@ -55,11 +86,62 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
                 // startActivity(new Intent(this, Register.class));
                 break;
             case R.id.bLogin:
-                Intent intentb= new Intent(this, HomeScreen.class);
-                startActivity(intentb);
+                signIn(v);
+                //Intent intentb= new Intent(this, HomeScreen.class);
+                //startActivity(intentb);
                 // startActivity(new Intent(this, Register.class));
                 break;
         }
+
+
+
+
+
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }}
+
+    public void signIn(View v){
+
+        final View view = v;
+        String email = etUsername.getText().toString();
+        String password = etPassword.getText().toString();
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "signInWithEmail:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "signInWithEmail", task.getException());
+                            System.out.println("fail");
+                            Toast.makeText(Login.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        else {
+
+
+                                Intent intent = new Intent(view.getContext() , HomeScreen.class);
+                                startActivity(intent);
+
+                        }
+
+                        // ...
+                    }
+                });
     }
 }
 
--- app/src/main/java/com/example/des/samplehomescreen/Register.java
+++ app/src/main/java/com/example/des/samplehomescreen/Register.java
@@ -3,13 +3,21 @@ package com.example.des.samplehomescreen;
 
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import com.example.des.studentmanagerredux.R;
+import android.widget.Toast;
 
+import com.example.des.studentmanagerredux.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 
 public class Register extends AppCompatActivity implements View.OnClickListener {
@@ -17,6 +25,10 @@ public class Register extends AppCompatActivity implements View.OnClickListener 
 
     Button bRegister;
     EditText etName, etAge, etUsername, etPassword;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private static final String TAG = "Login";
+
 
 
 
@@ -26,6 +38,21 @@ public class Register extends AppCompatActivity implements View.OnClickListener 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+                // ...
+            }
+        };
 
         etName = (EditText) findViewById(R.id.etName);
         etAge = (EditText) findViewById(R.id.etAge);
@@ -42,7 +69,8 @@ public class Register extends AppCompatActivity implements View.OnClickListener 
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        createAccount();
+        /*switch (v.getId()){
             //case R.id.bRegister:
 
 
@@ -54,8 +82,42 @@ public class Register extends AppCompatActivity implements View.OnClickListener 
                 startActivity(intent);
                 // startActivity(new Intent(this, Register.class));
                 break;
+        }*/
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
         }
     }
+
+public void createAccount() {
+    String email = etUsername.getText().toString();
+    String password = etPassword.getText().toString();
+    mAuth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    Log.d(TAG, "createUserWithEmail:onComplete:" + task.isSuccessful());
+
+                    // If sign in fails, display a message to the user. If sign in succeeds
+                    // the auth state listener will be notified and logic to handle the
+                    // signed in user can be handled in the listener.
+                    if (!task.isSuccessful()) {
+                        Toast.makeText(Register.this, "Authentication failed.",
+                                Toast.LENGTH_SHORT).show();
+                    }
+
+                    // ...
+                }
+            });
+}
 }
 
 
